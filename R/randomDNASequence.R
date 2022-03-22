cabee625b8ba104fd74717e9a498a3a50a0d0f60
Álvaro León <alvaroguleon@gmail.com>
2022-03-22 18:37:05 +0000

    Initial Vignette

--- R/randomDNASequence.R
+++ R/randomDNASequence.R
@@ -8,6 +8,6 @@
 #' @examples
 randomDNASequence <- function(length){
   seqVector <- sample(c("A", "T", "G", "C"), size = length, replace = TRUE)
-  seqString <- paste0(name_me3, collapse = "")
+  seqString <- paste0(seqVector, collapse = "")
   return(seqString)
 }
